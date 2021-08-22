05d650d0dc0ef5b6bec75ac6f12a077d7ebc99ba
himansh9489 <30984612+himansh9489@users.noreply.github.com>
2021-08-23 05:17:06 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Content from "./components/Content";
+import "./assets/scss/main.css";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Content></Content>
+    </>
+  );
+}
+
+export default App;
